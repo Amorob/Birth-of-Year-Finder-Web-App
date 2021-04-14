fc29281de6e97fc0b055184ec0730955a4dc142f
amoroabd <amoroabd@gmail.com>
2021-04-14 20:33:47 +0000

    Validations worked on

--- js/main.js
+++ js/main.js
@@ -5,13 +5,21 @@ currentYear = new Date().getFullYear();
 btn.addEventListener("click", function (e) {
   let ageInYears = document.getElementById("age").value;
   let birthYear = document.getElementById("birth-year");
+  if (isNaN(ageInYears)) {
+    alert("Enter a valid age");
+    return false;
+  } else if (ageInYears < 0) {
+    alert("Your age cannot be less than 0. Enter a valid age");
+    return false;
+  }
+
   birthYear.innerText = "You were born in " + (currentYear - ageInYears);
   birthYear.classList.add("birth-of-year");
   clearFields();
 
   e.preventDefault();
 });
-
+// Function clear fields
 function clearFields() {
   document.getElementById("age").value = "";
 }
